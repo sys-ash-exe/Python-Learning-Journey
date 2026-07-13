ea58694df46518f17ddbbe4adc0209cf70af699f
Ayan Ali <tobehonest2008@gmail.com>
2026-07-13 22:57:12 +0500

    Delete class1.py

--- class1.py
+++
@@ -1,6 +0,0 @@
-#INPUT/OUTPUT
-user_name = input("Enter your name")
-user_age = input("Enter your age")
-print("Welcome", user_name)
-print("Your name is:", user_name)
-print("Your name is:", user_age)
