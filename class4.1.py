a2954312fbaada6a9cf2b0eeb7361f3f8e76b79d
Ayan Ali <tobehonest2008@gmail.com>
2026-07-13 22:59:19 +0500

    Delete class4.1.py

--- class4.1.py
+++
@@ -1,8 +0,0 @@
-integer = int(input("Enter a number "))
-name = str(input("Enter your name "))
-if integer>=0:
-    print(name.upper()+","+" "+ "Your integer is positive")
-elif integer<0:
-    print(name.upper()+","+" "+ "Your integer is negative")
-else:
-    print("Invalid Output")
